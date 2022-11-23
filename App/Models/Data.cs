2150f1edf24251da2fb3fe5826d3a8237a883b8a
AEsir777 <lkb2438834817@gmail.com>
2022-11-23 00:26:49 -0500

    start the webapp

---
+++ App/Models/Data.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Genric;
+using System.Linq;
+using System.Threading.Task;
+
+namespace ASP.net_test.Models {
+    public class Data
+    {
+        
+    }
+}
